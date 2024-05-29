a42e6c335b81ec5a46a0d4931ad8048eb3778550
Derian <dmrodriguez2000@gmail.com>
2024-05-28 19:23:58 -0600

    create a new car

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,18 +1,26 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 
 import { CarsService } from './cars.service';
+import { Car } from './interfaces/car.interface';
+import { CreateCarDto } from './dtos';
 
 @Controller('cars')
+@UsePipes( ValidationPipe )
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
   @Get()
-  getAllCars() {
+  getAllCars(): Car[] {
     return this.carsService.findAll();
   }
 
   @Get(':id')
-  getCarById( @Param('id', ParseIntPipe) id: number ) {
+  getCarById( @Param('id', ParseUUIDPipe) id: string ): Car {
     return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar( @Body() createCarDto: CreateCarDto ) {
+    return this.carsService.create(createCarDto);
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,22 +1,24 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 
 import { Car } from './interfaces/car.interface';
+import { CreateCarDto } from './dtos';
 
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
     {
-        id: 1,
+        id: uuid(),
         brand: 'Toyota',
         model: 'Corolla' 
     },
     {
-        id: 2,
+        id: uuid(),
         brand: 'Honda',
         model: 'Civic' 
     },
     {
-        id: 3,
+        id: uuid(),
         brand: 'Jeep',
         model: 'Cherokee' 
     },
@@ -26,10 +28,21 @@ export class CarsService {
     return this.cars;
   }
 
-  findOneById( id: number ): Car {
-      const car = this.cars.find( car => car.id === id );
-      if ( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
-      
-      return car;
+  findOneById( id: string ): Car {
+    const car = this.cars.find( car => car.id === id );
+    if ( !car ) throw new NotFoundException(`Car with id '${ id }' not found`);
+    
+    return car;
+  }
+
+  create(createCarDto: CreateCarDto): Car {
+    const newCar: Car = {
+      id: uuid(),
+      ...createCarDto
+    }
+
+    this.cars.push(newCar);
+
+    return newCar;
   }
 }
