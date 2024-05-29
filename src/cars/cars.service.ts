adc7d88c598ddaa3aa17388d54f6319cea77d877
Derian <dmrodriguez2000@gmail.com>
2024-05-29 17:24:57 -0600

    create seed to populate cars and brands data

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -7,7 +7,7 @@ import { CreateCarDto, UpdateCarDto } from './dtos';
 @Injectable()
 export class CarsService {
   private cars: Car[] = [
-    {
+    /* {
         id: uuid(),
         brand: 'Toyota',
         model: 'Corolla' 
@@ -21,7 +21,7 @@ export class CarsService {
         id: uuid(),
         brand: 'Jeep',
         model: 'Cherokee' 
-    },
+    }, */
   ];
 
   findAll(): Car[] {
@@ -68,5 +68,9 @@ export class CarsService {
   delete( id: string ): void {
     this.findOneById( id );
     this.cars = this.cars.filter( car => car.id !== id );
+  }
+
+  fillCarsWithSeedData( cars: Car[] ) {
+    this.cars = cars;
   }
 }
